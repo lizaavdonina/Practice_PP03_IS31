6120f42338dddeed3e2caa27ab0de24f483d5910
lizaavdonina <elizavetaavdonina511@gmail.com>
2024-12-10 17:35:41 +0300

    Create ImageModul.py

---
+++ Src/2/ImageModul.py
@@ -0,0 +1,15 @@
+from PIL import Image
+class ImageProcessor:
+    def __init__(self):
+        self.image = None
+
+    def load_image(self, path):
+        self.image = Image.open(path)
+
+    def resize_image(self, width, height):
+        if self.image:
+            self.image = self.image.resize((width, height))
+
+    def rotate_image(self, angle):
+        if self.image:
+            self.image = self.image.rotate(angle, expand=True)
